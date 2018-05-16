ae40806e14cf312a403a38399d2568484c2dfa09
Oleksii Nikitin <o.nikitin@spd-ukraine.com>
2018-05-16 14:05:26 +0300

    sql import

--- src/main/java/my/web/application/config/HibernateConfig.java
+++ src/main/java/my/web/application/config/HibernateConfig.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 import java.util.Properties;
 
 @Configuration
@@ -21,10 +22,11 @@ public class HibernateConfig {
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("my.web.application.model");
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        hibernateProperties.setProperty("ibernate.use_sql_comments", "true");
+        hibernateProperties.setProperty("hibernate.use_sql_comments", "true");
         hibernateProperties.setProperty("validation-mode", "CALLBACK");//validation provider required
+        hibernateProperties.setProperty("hibernate.hbm2ddl.import_files_sql_extractor", "my.web.application.hibernate.CustomSqlExtractor");
         sessionFactory.setHibernateProperties(hibernateProperties);
         return sessionFactory;
     }
@@ -38,6 +40,7 @@ public class HibernateConfig {
         dataSource.setPassword("");
         dataSource.setInitialSize(5);
         dataSource.setMaxActive(10);
+        System.out.println(dataSource.getUrl());
         return dataSource;
     }
 
